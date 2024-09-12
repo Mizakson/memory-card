e9eb6fd4537bf755b8e55f161c6dcb17b5d242c4
Mizakson <maxwellizakson@gmail.com>
2024-09-12 14:02:23 -0400

    Add text to Description component

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,19 @@
 export default function Description() {
-    // ...
 
     return (
-        // ...
+        <header>
+            <div className="title">
+                <h1>Memory-card</h1>
+                <p>Odin Project React Course Project #2</p>
+                <p>A program by Mizakson</p>
+            </div>
+            <div className="rules">
+                <p>Click a picture and score a point, selecting an already clicked picture resets the game!</p>
+            </div>
+            <div className="score">
+                <p id="current">Score: </p>
+                <p id="best">Best Score: </p>
+            </div>
+        </header>
     )
 }
