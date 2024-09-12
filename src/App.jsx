24030c4aa35d76b69ec773e82e38faf316eb9783
Mizakson <maxwellizakson@gmail.com>
2024-09-12 13:55:11 -0400

    Import Description component -> App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 import './styles/App.css'
+import Description from './components/Description'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello...</h1>
+      <Description />
     </>
   )
 }
