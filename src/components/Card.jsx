2418ccf9e7c0cba5249e5d07014372883ad67a74
Mizakson <maxwellizakson@gmail.com>
2024-09-13 11:59:55 -0400

    Create initial Card, Display components

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    const keyId = crypto.randomUUID()
+    console.log(keyId)
+    const URL = "#"
+
+    return (
+        <div className="card" key={keyId}>
+            <img src={URL}/>
+            <p className="img-text"></p>
+        </div>
+    )
+}
