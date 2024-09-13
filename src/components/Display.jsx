bdf65b818cc02037cdd496f52b9f9d1f275db563
Mizakson <maxwellizakson@gmail.com>
2024-09-13 12:33:29 -0400

    Uninstall pexels library

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,6 @@
 import Card from "./Card";
-import { createClient } from "pexels";
 
 export default function Display() {
-
-    const client = createClient('YOUR_API_KEY');
-
-    console.log(client)
 
     return(
         <div className="display">
