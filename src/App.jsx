3a6103d33e4cbb053b13fd8631cc2aa34eb5ef15
Mizakson <maxwellizakson@gmail.com>
2024-09-16 15:18:01 -0400

    Clustered commit, testing API call

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,25 @@ const API_URL = 'https://api.com/pexels.com/v1/search'
 const API_KEY = import.meta.env.VITE_API_KEY
 
 function App() {
-  const [count, setScore] = useState(0)
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
+  const [picsArr, setPicsArr] = useState([])
+  const [cardInfo, setCardInfo] = useState([])
+
+  const search = 'animal'
 
   useEffect(() => {
     // code to be executed
+    fetch(`${API_URL}?query=${search}&per_page=15&page=1`, {
+      mode: 'cors',
+      headers: {
+        Authorization: API_KEY
+      }
+    })
+      .then((response) => response.json())
 
     // optional return function
 }, []) // dependency array
-
-  console.log(API_KEY)
-  console.log(API_URL)
 
   return (
     <>
