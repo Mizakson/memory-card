0da6d82530ad51747edaf496510d7d67e62149a4
Mizakson <maxwellizakson@gmail.com>
2024-09-16 15:12:04 -0400

    Update .gitignore, add API constants

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Card from './components/Card'
 import Description from './components/Description'
 import Score from './components/Score'
 
+// API KEY + URL
+const API_URL = 'https://api.com/pexels.com/v1/search'
+const API_KEY = import.meta.env.VITE_API_KEY
+
 function App() {
   const [count, setScore] = useState(0)
 
@@ -12,6 +16,9 @@ function App() {
 
     // optional return function
 }, []) // dependency array
+
+  console.log(API_KEY)
+  console.log(API_URL)
 
   return (
     <>
